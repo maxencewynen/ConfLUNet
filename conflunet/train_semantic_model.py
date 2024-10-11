b6bce83dd1e126350f1319c9c4d1c12cf97f394e
Maxence Wynen <maxencewynen@gmail.com>
2024-10-11 18:30:40 +0200

    refactor: rearrange stuff so as to shorten the main loops

--- conflunet/train_semantic_model.py
+++ conflunet/train_semantic_model.py
@@ -9,7 +9,7 @@ from conflunet.dataloading.dataloaders import (
     get_train_dataloader_from_dataset_id_and_fold,
     get_val_dataloader_from_dataset_id_and_fold
 )
-from conflunet.training.utils import get_default_device, seed_everything
+from conflunet.training.utils import get_default_device, seed_everything, save_patch
 import wandb
 from os.path import join as pjoin
 from conflunet.evaluation.semantic_segmentation import dice_metric, dice_norm_metric
@@ -53,45 +53,18 @@ parser.add_argument('--save_predictions', default=False, action='store_true',
                     help="Debug mode: save predictions of the first batch for each validation step")
 
 
-def save_patch(data, name, save_dir):
-    import nibabel as nib
-    import numpy as np
-    tobesaved = data if len(data.shape) == 3 else np.squeeze(data[0,0,:,:,:])
-    nib.save(nib.Nifti1Image(tobesaved, np.eye(4)),
-            pjoin(save_dir, f'{name}.nii.gz'))
-
-
 def main(args):
     seed_val = args.seed
     seed_everything(seed_val)
-
     device = get_default_device()
     torch.multiprocessing.set_sharing_strategy('file_system')
 
+    ### Dataset configuration ###
     dataset_name = convert_id_to_dataset_name(args.dataset_id)
     plans_file = pjoin(nnUNet_preprocessed, dataset_name, 'nnUNetPlans.json')
     plans_manager = PlansManagerInstanceSeg(plans_file)
     configuration = plans_manager.get_configuration('3d_fullres')
     n_channels = len(plans_manager.foreground_intensity_properties_per_channel)
-
-    args.name = f"SEMANTIC_{args.name}"
-    if args.debug:
-        args.name = f"DEBUG_{args.name}"
-
-    save_dir = pjoin(nnUNet_results, dataset_name, args.name, 'fold_%d' % args.fold)
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
-
-    if args.save_predictions or args.debug:
-        patches_save_dir = pjoin(save_dir, 'saved_patches')
-        os.makedirs(patches_save_dir, exist_ok=True)
-
-    checkpoint_filename = os.path.join(save_dir, f"checkpoint_final.pth")
-    model, optimizer, lr_scheduler, start_epoch, wandb_run_id = \
-        get_model_optimizer_and_scheduler(args, configuration, n_channels, device, checkpoint_filename, semantic=True)
-
-    loss_fn = SemanticSegmentationLoss(dice_loss_weight=args.dice_loss_weight,
-                                                          focal_loss_weight=args.focal_loss_weight)
 
     # Initialize dataloaders
     train_loader = get_train_dataloader_from_dataset_id_and_fold(args.dataset_id, args.fold,
@@ -109,24 +82,46 @@ def main(args):
                                                                  cache_rate=args.cache_rate,
                                                                  seed_val=args.seed)
 
+    args.name = f"SEMANTIC_{args.name}"
+    if args.debug:
+        args.name = f"DEBUG_{args.name}"
+
+    save_dir = pjoin(nnUNet_results, dataset_name, args.name, 'fold_%d' % args.fold)
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+
+    if args.save_predictions or args.debug:
+        patches_save_dir = pjoin(save_dir, 'saved_patches')
+        os.makedirs(patches_save_dir, exist_ok=True)
+
+    ### Model and training configuration ###
+    checkpoint_filename = os.path.join(save_dir, f"checkpoint_final.pth")
+    model, optimizer, lr_scheduler, start_epoch, wandb_run_id = \
+        get_model_optimizer_and_scheduler(args, configuration, n_channels, device, checkpoint_filename, semantic=True)
+
+    loss_fn = SemanticSegmentationLoss(dice_loss_weight=args.dice_loss_weight,
+                                       focal_loss_weight=args.focal_loss_weight)
+
     # Initialize other variables and metrics
     epoch_num = args.n_epochs
     val_interval = args.val_interval
     best_val_loss = np.inf
-    epoch_loss_values, metric_values_nDSC, metric_values_DSC = [], [], []
-    best_dsc = -np.inf
-    best_ndsc = -np.inf
-
-    # Initialize scaler
+    best_metrics = {metric: -np.inf for metric in ["Validation Metrics/Dice Score", "Validation Metrics/Normalized Dice"]}
     scaler = torch.cuda.amp.GradScaler()
 
-    ''' Training loop '''
+    #############################
+    ###     Training Loop     ###
+    #############################
     for epoch in range(start_epoch, epoch_num):
+        n_training_batches = len(train_loader)
         start_epoch_time = time.time()
-        print("-" * 10)
-        print(f"epoch {epoch + 1}/{epoch_num}")
+        print("-" * 10, f"\nepoch {epoch + 1}/{epoch_num}")
         model.train()
-        epoch_loss = epoch_loss_ce = epoch_loss_dice = 0
+        epoch_loss_values = {
+            'Training Loss/Total Loss': 0,
+            'Training Segmentation Loss/Dice Loss': 0,
+            'Training Segmentation Loss/Focal Loss': 0,
+        }
 
         for batch_idx, batch_data in enumerate(train_loader):
             start_batch_time = time.time()
@@ -146,50 +141,47 @@ def main(args):
 
             loss, dice_loss, focal_loss = loss_fn(semantic_pred, labels)
 
-            epoch_loss += loss.item()
-            epoch_loss_ce += focal_loss.item()
-            epoch_loss_dice += dice_loss.item()
+            for key, loss in zip(epoch_loss_values.keys(), [loss, dice_loss, focal_loss]):
+                epoch_loss_values[key] += loss.item()
 
             scaler.scale(loss).backward()
-
             scaler.step(optimizer)
             scaler.update()
-
             optimizer.zero_grad()
-
             elapsed_time = time.time() - start_batch_time
-            print(
-                f"Batch {batch_idx + 1}/{len(train_loader)}, train_loss: {loss.item():.4f} " 
-                f"(elapsed time: {int(elapsed_time // 60)}min {int(elapsed_time % 60)}s)")
+
+            print(f"Batch {batch_idx + 1}/{len(train_loader)}, train_loss: {loss.item():.4f} "
+                  f"(elapsed time: {int(elapsed_time // 60)}min {int(elapsed_time % 60)}s)")
+
+        for key in epoch_loss_values:
+            epoch_loss_values[key] /= n_training_batches
 
         elapsed_epoch_time = time.time() - start_epoch_time
         print(f"Epoch {epoch + 1} took {int(elapsed_epoch_time // 60)}min {int(elapsed_epoch_time % 60)}s")
-
-        epoch_loss /= len(train_loader)
-        epoch_loss_dice /= len(train_loader)
-        epoch_loss_ce /= len(train_loader)
-        epoch_loss_values.append(epoch_loss)
-
-        print(f"Epoch {epoch + 1} average loss: {epoch_loss:.4f}")
-
-        current_lr = optimizer.param_groups[0]['lr']
-        lr_scheduler.step()
+        print(f"Epoch {epoch + 1} average loss: {epoch_loss_values['Training Loss/Total Loss']:.4f}")
 
         if not args.wandb_ignore:
-            wandb.log(
-                {'Training Loss/Total Loss': epoch_loss, 'Training Loss/Dice Loss': epoch_loss_dice,
-                 'Training Loss/Focal Loss': epoch_loss_ce, 'Learning rate': current_lr},
-                step=epoch)
+            wandb.log({**epoch_loss_values, **{'Learning rate': optimizer.param_groups[0]['lr']}}, step=epoch)
 
-        ##### Validation #####
+        lr_scheduler.step()
+
+        #############################
+        ###    Validation Loop    ###
+        #############################
         if (epoch + 1) % val_interval == 0:
             start_validation_time = time.time()
+            avg_val_metrics = {
+                'Validation Loss/Total Loss': 0,
+                'Validation Segmentation Loss/Dice Loss': 0,
+                'Validation Segmentation Loss/Focal Loss': 0,
+                'Validation Metrics/Dice Score': 0,
+                'Validation Metrics/Normalized Dice': 0,
+            }
+            batch_size = 0
+            n_batches = len(val_loader)
             model.eval()
-            with torch.no_grad():
-                avg_val_loss = avg_val_dice_loss = avg_val_ce_loss = 0
-                total_dice = total_ndsc = 0
-                batch_size = 0
 
+            with torch.no_grad():
                 for batch_idx, val_data in enumerate(val_loader):
                     val_inputs, val_labels = (
                         val_data["img"].to(device),
@@ -213,51 +205,40 @@ def main(args):
                         save_patch(binary_seg[0], f"{epoch}_pred_segmentation_binary", patches_save_dir)
 
                     batch_size = val_labels.shape[0]
+                    pred = val_labels.cpu().numpy()
                     for mbidx in range(batch_size):
-                        total_dice += dice_metric(np.squeeze(val_labels.cpu().numpy()[mbidx]), binary_seg[mbidx])
-                        total_ndsc += dice_norm_metric(np.squeeze(val_labels.cpu().numpy()[mbidx]), binary_seg[mbidx])
+                        avg_val_metrics["Validation Metrics/Dice Score"] += dice_metric(np.squeeze(pred[mbidx]), binary_seg[mbidx])
+                        avg_val_metrics["Validation Metrics/Normalized Dice"] += dice_norm_metric(np.squeeze(pred[mbidx]), binary_seg[mbidx])
 
                     val_loss, val_dice_loss, val_focal_loss = loss_fn(val_semantic_pred, val_labels)
 
-                    avg_val_loss += val_loss.item()
-                    avg_val_dice_loss += val_dice_loss.item()
-                    avg_val_ce_loss += val_focal_loss.item()
+                    for key, loss in zip(avg_val_metrics.keys(), res):
+                        if "Loss" in key:
+                            avg_val_metrics[key] += loss.item()
 
-                avg_val_loss /= len(val_loader)
-                avg_val_dice_loss /= len(val_loader)
-                avg_val_ce_loss /= len(val_loader)
-                total_dice /= (len(val_loader) * batch_size)
-                total_ndsc /= (len(val_loader) * batch_size)
+                for key in avg_val_metrics:
+                    avg_val_metrics[key] /= n_batches * batch_size if "Metrics" in key else n_batches
 
                 if not args.wandb_ignore:
-                    wandb.log(
-                        {'Validation Loss/Total Loss': avg_val_loss, 'Validation Loss/Dice Loss': avg_val_dice_loss,
-                         'Validation Segmentation Loss/Focal Loss': avg_val_ce_loss,
-                         'Validation Metrics/Dice Score': total_dice,
-                         'Validation Metrics/Normalized Dice': total_ndsc},
-                        step=epoch
-                    )
+                    wandb.log(avg_val_metrics, step=epoch)
 
-                if avg_val_loss < best_val_loss:
-                    best_val_loss = avg_val_loss
+                if avg_val_metrics["Validation Loss/Total Loss"] < best_val_loss:
+                    best_val_loss = avg_val_metrics["Validation Loss/Total Loss"]
                     save_path = os.path.join(save_dir, f"best_loss.pth")
                     torch.save(model.state_dict(), save_path)
 
-                if total_ndsc > best_ndsc:
-                    best_ndsc = total_ndsc
-                    save_path = os.path.join(save_dir, f"best_ndsc.pth")
-                    torch.save(model.state_dict(), save_path)
-
-                if total_dice > best_dsc:
-                    best_dsc = total_dice
-                    save_path = os.path.join(save_dir, f"best_dsc.pth")
-                    torch.save(model.state_dict(), save_path)
+                for metric in best_metrics.keys():
+                    if avg_val_metrics[metric] > best_metrics[metric]:
+                        best_metrics[metric] = avg_val_metrics[metric]
+                        name = metric.replace('Validation Metrics/', '').replace(' ', '_')
+                        save_path = os.path.join(save_dir, f"best_{name}.pth")
+                        torch.save(model.state_dict(), save_path)
 
                 val_elapsed_time = time.time() - start_validation_time
-                print(f"Validation took {int(val_elapsed_time // 60)}min {int(val_elapsed_time % 60)}s")
-                print(f"Validation Loss: {avg_val_loss:.4f}")
-                print(f"Validation DSC: {total_dice:.4f} (best DSC: {best_dsc:.4f})")
-                print(f"Validation nDSC: {total_ndsc:.4f} (best nDSC: {best_ndsc:.4f})")
+                print(f"Validation took {int(val_elapsed_time // 60)}min {int(val_elapsed_time % 60)}s\n"
+                      f"Validation Loss/Total Loss: {avg_val_metrics['Validation Loss/Total Loss']:.4f}")
+                for key in avg_val_metrics:
+                    print(f"{key}: {avg_val_metrics[key]:.4f}")
 
         if not args.debug and not args.wandb_ignore:
             torch.save({
