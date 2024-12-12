87656a8620a24f0d481d832319dc7198b9258a9c
Maxence Wynen <maxencewynen@gmail.com>
2024-12-12 17:20:25 +0100

    yes

--- conflunet/training/losses.py
+++ conflunet/training/losses.py
@@ -152,6 +152,7 @@ class ConfLUNetLoss(WeightedConfLUNetLoss):
             offsets_weights: torch.Tensor = None,
             centers_weights: torch.Tensor = None,
     ):
+        breakpoint()
         if semantic_weights is not None:
             warnings.warn("ConfLUNetLoss does not support semantic_weights. Ignoring them.")
         if offsets_weights is not None:
@@ -162,3 +163,147 @@ class ConfLUNetLoss(WeightedConfLUNetLoss):
                                                    semantic_ref, center_heatmap_ref, offsets_ref,
                                                    semantic_weights=None, offsets_weights=None, 
                                                    centers_weights=None)
+
+
+class PullLoss(Callable):
+    def __call__(
+            self,
+            offsets_pred: torch.Tensor,
+            associative_pred: torch.Tensor,
+            semantic_ref: torch.Tensor,
+            instance_seg_ref: torch.Tensor,
+    ):
+        ### BUILD 4D EMBEDDING ###
+        # concatenate (position matrix + offsets) and associative pred
+        complete_embedding = ...  # (batch_size, 4, H, W, D)
+
+        # Useful for later: build a 4d instance reference mask where the ids are repeated along the associative
+        # dimension (3 spatial + 1 associative)
+        instance_seg_ref_plus_associative = ...
+
+        total_pull_loss = 0
+
+        all_instances_id = torch.unique(instance_seg_ref)
+        all_instances_id = all_instances_id[all_instances_id != 0]
+        n_instances = len(all_instances_id)
+
+        # For each instance in the reference:
+        for instance_id in all_instances_id:
+            # Have a binary mask for this instance
+            this_instance_mask = instance_seg_ref_plus_associative == instance_id
+
+            # Compute centroid coordinate (4d tensor), which corresponds to the average of all embedding coordinates
+            # of the instances voxels
+            this_instance_centroid_coordinate = ...
+
+            # Get list of 4d embedding coordinates for this instance
+            this_instance_embeddings_coordinates = ...  # (n_voxels_in_this_instance, 4)
+
+            # This step can be done in multiple chunks if GPU memory is needed!
+            # Average all squared L2 norm of (embedding coordinate - this instance centroid coordinate) for all voxels
+            this_instance_pull_loss = MSELoss()(this_instance_embeddings_coordinates,
+                                                this_instance_centroid_coordinate.expand_as(this_instance_embeddings_coordinates))
+
+        total_pull_loss = total_pull_loss / n_instances if n_instances > 0 else torch.tensor(0).to(offsets_pred.device)
+
+        return total_pull_loss
+
+
+class PushLoss(Callable):
+    def __init__(self, margin: float = 4):
+        super(PushLoss, self).__init__()
+        self.margin = margin
+
+    def __call__(
+            self,
+            offsets_pred: torch.Tensor,
+            associative_pred: torch.Tensor,
+            semantic_ref: torch.Tensor,
+            instance_seg_ref: torch.Tensor,
+    ):
+        # Compute spatial embedding
+        spatial_embedding = ...  # position matrix + offsets pred
+
+        total_push_loss = 0
+
+        all_instances_id = torch.unique(instance_seg_ref)
+        all_instances_id = all_instances_id[all_instances_id != 0]
+        n_instances = len(all_instances_id)
+
+        spatial_centroids = {}
+        associative_centroids = {}
+        # compute the spatial and associative centroids of each instance
+        for instance_id in all_instances_id:
+            # Have a binary mask for this instance
+            this_instance_mask_spatial = instance_seg_ref == instance_id
+
+
+            spatial_centroids[instance_id] = ...
+
+            this_instance_mask_associative = instance_seg_ref == instance_id
+            associative_centroids[instance_id] = ...
+
+        for instance_id_1 in all_instances_id:
+            spatial_centroid_1 = spatial_centroids[instance_id_1]
+            associative_centroid_1 = associative_centroids[instance_id_1]
+
+            for instance_id_2 in all_instances_id[all_instances_id != instance_id_1]:
+                spatial_centroid_2 = spatial_centroids[instance_id_2]
+                associative_centroid_2 = associative_centroids[instance_id_2]
+
+                # L2 norm between spatial centroids
+                spatial_distance = torch.norm(spatial_centroid_1 - spatial_centroid_2)
+
+                # compute the right margin for this pair of instances
+                associative_margin = torch.sqrt(self.margin ** 2 - spatial_distance ** 2)  # ?????
+
+                associative_distance = torch.norm(associative_centroid_1 - associative_centroid_2)
+                push_loss = torch.relu(torch.abs(associative_margin - associative_distance) ** 2)
+
+                total_push_loss += push_loss
+
+        total_push_loss = total_push_loss / (n_instances * (n_instances - 1)) if n_instances > 0 \
+            else torch.tensor(0).to(offsets_pred.device)
+
+        return total_push_loss
+
+
+class HybridLoss(Callable):
+    def __init__(
+            self,
+            segmentation_loss_weight: float = 1.0,
+            embedding_loss_weight: float = 0.01, # lambda_emb
+            loss_function_pull: Callable = PullLoss(),
+            loss_function_push: Callable = PushLoss(),
+            loss_function_segmentation: Callable = WeightedSemanticSegmentationLoss(),
+            margin: float = 4,
+    ):
+        self.segmentation_loss_weight = segmentation_loss_weight
+        self.embedding_loss_weight = embedding_loss_weight
+        self.loss_function_segmentation = loss_function_segmentation
+        self.loss_function_pull = loss_function_pull
+        self.loss_function_push = loss_function_push
+        self.margin = margin
+
+    def __call__(
+            self,
+            semantic_pred: torch.Tensor,
+            associative_pred: torch.Tensor,
+            offsets_pred: torch.Tensor,
+            semantic_ref: torch.Tensor,
+            spatial_centers_ref: torch.Tensor,
+            instance_seg_ref: torch.Tensor,
+            semantic_weights: torch.Tensor = None,
+            offsets_weights: torch.Tensor = None,
+            centers_weights: torch.Tensor = None,
+    ):
+        segmentation_loss, dice_loss, focal_loss = self.loss_function_segmentation(semantic_pred, semantic_ref, semantic_weights)
+
+        loss_pull = self.loss_function_pull(offsets_pred, associative_pred, semantic_ref, instance_seg_ref)
+        loss_push = self.loss_function_push(offsets_pred, associative_pred, semantic_ref, instance_seg_ref)
+        loss_embedding = loss_pull + loss_push
+
+        loss = (self.segmentation_loss_weight * segmentation_loss +
+                self.embedding_loss_weight * loss_embedding)
+
+        return loss, dice_loss, focal_loss, segmentation_loss, loss_pull, loss_push, loss_embedding
