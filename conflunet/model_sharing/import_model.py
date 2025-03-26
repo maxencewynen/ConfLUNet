4352bd90887d1a1726aba5b28aedab991943c5cd
Maxence Wynen <maxencewynen@gmail.com>
2025-03-26 12:22:02 +0100

    fix: import/export models

--- conflunet/model_sharing/import_model.py
+++ conflunet/model_sharing/import_model.py
@@ -10,9 +10,14 @@ from nnunetv2.paths import nnUNet_preprocessed, nnUNet_results, nnUNet_raw
 def import_model(compressed_file):
     assert compressed_file.endswith('.zip'), f"Expected export file to be a .zip file but got .{compressed_file.split('.')[-1]}"
     dirname = os.path.dirname(compressed_file)
-    shutil.unpack_archive(compressed_file, dirname, 'zip')
-
     cpf = compressed_file.replace('.zip', '')
+    if not pexists(cpf):
+        os.makedirs(cpf)
+    else:
+        cpf += '_1'
+        os.makedirs(cpf)
+    shutil.unpack_archive(compressed_file, cpf, 'zip')
+
     dataset_name = None
     for i, dataset_name in enumerate(os.listdir(pjoin(dirname, cpf, 'nnUNet_results'))):
         if i > 0:
@@ -25,6 +30,11 @@ def import_model(compressed_file):
 
     shutil.move(pjoin(dirname, cpf, 'nnUNet_results', dataset_name, model_name),
                 pjoin(nnUNet_results, dataset_name))
+    if pexists(pjoin(nnUNet_raw, dataset_name, 'dataset.json')):
+        r = input(f"Path '{pjoin(nnUNet_raw, dataset_name, 'dataset.json')}' already exists. Replace (y/n)?")
+        if r.lower() not in ('n', 'no'):
+            shutil.move(pjoin(dirname, cpf, 'nnUNet_raw', dataset_name, 'dataset.json'),
+                        nnUNet_raw, dataset_name)
     if pexists(pjoin(nnUNet_preprocessed, dataset_name, 'dataset.json')):
         r = input(f"Path '{pjoin(nnUNet_preprocessed, dataset_name, 'dataset.json')}' already exists. Replace (y/n)?")
         if r.lower() not in ('n', 'no'):
